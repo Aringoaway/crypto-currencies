a6a888319b8d1aa705bb093d463186c2698bf6b0
Aringoaway <a.konshina.job@gmail.com>
2022-03-23 10:37:16 +0100

    add useState, useParams and useEffect for join id for page AboutCoin

--- src/Pages/AboutCoin.js
+++ src/Pages/AboutCoin.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {useParams} from "react-router-dom";
+import axios from "axios";
+import {SingleCoin} from "../config/api";
 
 const AboutCoin = () => {
+    const { id } = useParams();
+    const [coin, setCoin] = useState();
+
+    const fetchCoin = async () => {
+        const { data } = await axios.get(SingleCoin(id));
+        setCoin(data);
+    }
+
+    console.log(coin);
+
+    useEffect(() => {
+        fetchCoin();
+    }, []);
+
+
+
     return (
         <div>
-            about coin
+            about coin page
         </div>
     )
 }
