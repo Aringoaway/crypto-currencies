c7c1727ce915e225fdce5b37de41fdb72596943d
Aringoaway <a.konshina.job@gmail.com>
2022-03-23 12:34:18 +0100

    add fetch to Coin info chart

--- src/Components/CoinInfo.js
+++ src/Components/CoinInfo.js
@@ -1,6 +1,23 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {HistoricalChart} from "../config/api";
+import axios from "axios";
 
-const CoinInfo = () => {
+const CoinInfo = ({coin}) => {
+    const [hisloricData, setHistoricalData] = useState();
+    const [days, setDays] = useState(1);
+
+    const fetchHistoricData = async () => {
+        const { data } = await axios.get(HistoricalChart(coin.id, days));
+
+        setHistoricalData(data.prices)
+    }
+
+    useEffect(() => {
+        fetchHistoricData();
+    }, [days]);
+
+
+
     return(
         <div>Coin info</div>
     )
